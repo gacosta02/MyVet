cb93bd174d8b70529324a93f70c777e31bdaccf4
Gabriel Eduardo Acosta Nuñez <gacosta.com.do@outlook.com>
2019-09-02 00:49:22 -0400

    createDB

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+        [Required]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+        public string Adress { get; set; }
+        //propiedad de solo lectura
+        //public string FullName {
+        //    get
+        //    {
+        //        return $"{FirstName} {LastName}";
+        //    }
+        //     }
+        public string FullNme => $"{FirstName} {LastName}";
+        public string FullNameWithDocumente => $"{FirstName}{LastName}-{Document}";
+
+
+    }
+}
---
+++ MyVet.Web/Migrations/20190902044735_InitialDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyVet.Web.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Document = table.Column<string>(maxLength: 30, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    FixedPhone = table.Column<string>(nullable: true),
+                    CellPhone = table.Column<string>(nullable: false),
+                    Adress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "owners");
+        }
+    }
+}
